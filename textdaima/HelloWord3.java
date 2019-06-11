e4eb7d5fc6fa999b146a61a55afc6d90bdfad47d
jizhongwen123 <1448018976@qq.com>
2019-06-11 18:06:11 +0800

    实训第一天，11号

---
+++ textdaima/HelloWord3.java
@@ -0,0 +1,14 @@
+public  class HelloWord{
+    public  static void main(String args[]){
+       System.out.println("HelloWord 1");
+        System.out.println("HelloWord 2");
+}
+}
+
+
+public   class HelloWord1{
+    public  static void main(String args[]){
+       System.out.println("HelloWord 1");
+        System.out.println("HelloWord 2");
+}
+}
